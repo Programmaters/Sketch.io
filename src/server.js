3e18dc4a282e8144729303a0aa8b6a674b50fbd2
Ricardo <76919550+R1c4rdCo5t4@users.noreply.github.com>
2023-07-19 18:59:34 +0100

    Project Refactoring

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import { Server } from "socket.io"
+
+const io = new Server(3001)
+
+io.on("connection", (socket) => {
+
+    // send a message to the client
+    socket.emit("onJoinLobby", "Welcome to the lobby!")
+
+    // receive a message from the client
+    socket.on("onReceiveGuess", (...args) => {
+        console.log(args)
+    })
+})
