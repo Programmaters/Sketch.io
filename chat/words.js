73f235f90fa2a47c231958466fceaaa81bdae4e6
Daniel Sousa <danielcabritasousa@hotmail.com>
2023-07-24 18:50:32 +0100

    Update words.js

--- chat/words.js
+++ chat/words.js
@@ -1,25 +1,18 @@
-function compareTwoWords(first, second) {
-	first = first.replace(/\s+/g, '')
-	second = second.replace(/\s+/g, '')
-
-	if (first === second) return 1 // identical or empty
-	if (first.length < 2 || second.length < 2) return 0 // if either is a 0-letter or 1-letter string
-
-	let firstBigrams = new Map()
-	for (let i = 0; i < first.length - 1; i++) {
-		const bigram = first.substring(i, i + 2)
-		const count = firstBigrams.has(bigram) ? firstBigrams.get(bigram) + 1 : 1
-		firstBigrams.set(bigram, count)
+function compareTwoWords(str1, str2) {
+	const n = Math.max(str1.length, str2.length)
+	let hammingDistance = 0
+  
+	for (let i = 0; i < n; i++) {
+	  const char1 = str1[i]
+	  const char2 = str2[i]
+	  
+	  if (char1 !== char2) {
+		hammingDistance++
+	  }
 	}
-
-	let intersectionSize = 0
-	for (let i = 0; i < second.length - 1; i++) {
-		const bigram = second.substring(i, i + 2)
-		const count = firstBigrams.has(bigram) ? firstBigrams.get(bigram) : 0
-		if (count > 0) {
-			firstBigrams.set(bigram, count - 1)
-			intersectionSize++
-		}
-	}
-	return (2.0 * intersectionSize) / (first.length + second.length - 2)
+  
+	const normalizedDistance = hammingDistance / n
+  
+	return 1 - normalizedDistance
 }
+  
